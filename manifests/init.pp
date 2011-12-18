26752bc8a3751acf9a225df67d5670bfbcd8d0ef
Alessandro Franceschi <al@lab42.it>
2011-12-19 00:45:30 +0100

    Wordpress module upgraded to version 2.0.1. Still missing rspec tests and tests on http server integration

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,29 +12,69 @@
 #     - package : Installs wordpress using the OS common packages
 #     - source  : Installs wordpress downloading and extracting a specific tarball or zip file
 #     - puppi   : Installs wordpress tarball or file via Puppi, creating the "puppi deploy wordpress" command
+#   Can be defined also by the (top scope) variable $wordpress_install
 #
 # [*install_source*]
 #   The URL from where to retrieve the source tarball/zip. Used if install => "source" or "puppi"
 #   Default is from upstream developer site. Update the version when needed.
+#   Can be defined also by the (top scope) variable $wordpress_install_source
 #
 # [*install_destination*]
 #   The base path where to extract the source tarball/zip. Used if install => "source" or "puppi"
 #   By default is the distro's default DocumentRoot for Web or Application server
+#   Can be defined also by the (top scope) variable $wordpress_install_destination
 #
 # [*install_precommand*]
 #   A custom command to execute before installing the source tarball/zip. Used if install => "source" or "puppi"
 #   Check wordpress/manifests/params.pp before overriding the default settings
+#   Can be defined also by the (top scope) variable $wordpress_install_precommand
 #
 # [*install_postcommand*]
 #   A custom command to execute after installing the source tarball/zip. Used if install => "source" or "puppi"
 #   Check wordpress/manifests/params.pp before overriding the default settings
+#   Can be defined also by the (top scope) variable $wordpress_install_postcommand
 #
 # [*url_check*]
 #   An url, relevant to the wordpress application, to use for testing the correct deployment of wordpress.
 #   Used is monitor is enabled.
+#   Can be defined also by the (top scope) variable $wordpress_url_check
 # 
 # [*url_pattern*]
 #   A string that must exist in the defined url_check that confirms that the application is running correctly
+#   Can be defined also by the (top scope) variable $wordpress_url_pattern
+#
+# [*web_server*]
+#   The type of web server you want to preconfigure. (Possible values: apache, lighttpd, nginx) 
+#   Can be defined also by the (top scope) variable $wordpress_web_server
+#
+# [*web_server_template*]
+#   The path of the template to use for web server configuration
+#   Can be defined also by the (top scope) variable $wordpress_web_server_template
+#
+# [*web_virtualhost*]
+#   An optional virtualhost name to map to the wordpress application
+#   Can be defined also by the (top scope) variable $wordpress_web_virtualhost
+#
+# [*db_type*]
+#   Database type to use. Default: mysql
+#   Can be defined also by the (top scope) variable $wordpress_db_type
+#
+# [*db_name*]
+#   Name of the database to create
+#   Can be defined also by the (top scope) variable $wordpress_db_name
+#
+# [*db_host*]
+#   Your database server hostname. Default: localhost
+#   If you define an external db server and want to configure it automatically, you need to have StoredConfigs activated.
+#   Can be defined also by the (top scope) variable $wordpress_db_host
+#
+# [*db_user*]
+#   The user wordpress uses to connect to the database
+#   Can be defined also by the (top scope) variable $wordpress_db_user
+#
+# [*db_password*]
+#   The password used by db_user. Default is a random value (that doesn't change in different Puppet runs)
+#   Can be defined also by the (top scope) variable $wordpress_db_password
 #
 #
 # Standard class parameters - Define the general class behaviour and customizations
@@ -42,44 +82,68 @@
 # [*my_class*]
 #   Name of a custom class to autoload to manage module's customizations 
 #   If defined, wordpress class will automatically "include $my_class"
+#   Can be defined also by the (top scope) variable $wordpress_myclass
 #
 # [*source*]
 #   Sets the content of source parameter for main configuration file
 #   If defined, wordpress main config file will have the parameter: source => $source
+#   Can be defined also by the (top scope) variable $wordpress_source
 #
 # [*source_dir*]
 #   If defined, the whole wordpress configuration directory content is retrieved recursively from
 #   the specified source (parameter: source => $source_dir , recurse => true)
+#   Can be defined also by the (top scope) variable $wordpress_source_dir
 #
 # [*source_dir_purge*]
-#   If set to true all the existing configuration directory is overriden by the 
-#   content retrived from source_dir. (source => $source_dir , recurse => true , purge => true) 
+#   If set to true (default false)  the existing configuration directory is mirrored with the 
+#   content retrieved from source_dir. (source => $source_dir , recurse => true , purge => true) 
+#   Can be defined also by the (top scope) variable $wordpress_source_dir_purge
 #
 # [*template*]
 #   Sets the path to the template to be used as content for main configuration file
 #   If defined, wordpress main config file will have: content => content("$template")
 #   Note source and template parameters are mutually exclusive: don't use both
+#   Can be defined also by the (top scope) variable $wordpress_template
 #
 # [*options*]
 #   An hash of custom options that can be used in templates for arbitrary settings.
+#   Can be defined also by the (top scope) variable $wordpress_options
 # 
 # [*absent*] 
 #   Set to 'true' to remove package(s) installed by module 
+#   Can be defined also by the (top scope) variable $wordpress_absent
 #
 # [*monitor*]
 #   Set to 'true' to enable monitoring of the services provided by the module
+#   Can be defined also by the (top scope) variables $wordpress_monitor and $monitor
 #
 # [*monitor_tool*]
 #   Define which monitor tools (ad defined in Example42 monitor module) you want to use for wordpress
+#   Can be defined also by the (top scope) variables $wordpress_monitor_tool and $monitor_tool
+#
+# [*monitor_target*]
+#   The Ip address or hostname to use as a target for monitoring tools. Default is the fact $ip_address
+#   Can be defined also by the (top scope) variables $wordpress_monitor_target and $monitor_target
 #
 # [*puppi*]
 #   Set to 'true' to enable creation of module data files that are used by puppi
+#   Can be defined also by the (top scope) variables $wordpress_puppi and $puppi
+#
+# [*puppi_helper*]
+#   Specify the helper to use for puppi commands. The default for this module is specified in params.pp
+#   and is generally a good choice. You can customize the output of puppi commands for this module
+#   using a different puppi helper. Use the define puppi::helper to create a new custom helper
+#   Can be defined also by the (top scope) variables $wordpress_puppi_helper and $puppi_helper
 #
 # [*debug*]
 #   Set to 'true' to enable modules debugging
+#   Can be defined also by the (top scope) variables $wordpress_debug and $debug
 #
+# [*audit_only*]
+#   Set to 'true' if you don't intend to override existing configuration files and want to audit the
+#   difference between existing files and the ones that Puppet would provide
+#   Can be defined also by the (top scope) variables $wordpress_audit_only and $audit_only
 # 
-# Default class params - As defined in wordpress::params.
 # Note that these variables are mostly defined and used in the module itself, overriding the default
 # values might not affected all the involved components (ie: packages layout)
 # Set and override them only if you know what you're doing.
@@ -128,17 +192,32 @@ class wordpress (
   $install_postcommand = $wordpress::params::install_postcommand,
   $url_check           = $wordpress::params::url_check,
   $url_pattern         = $wordpress::params::url_pattern,
-  $my_class            = "",
-  $source              = "",
-  $source_dir          = "",
-  $source_dir_purge    = false,
-  $template            = "",
-  $options             = "",
-  $absent              = false,
-  $monitor             = false,
-  $monitor_tool        = "",
-  $puppi               = false,
-  $debug               = false,
+  $web_server          = $wordpress::params::web_server,
+  $web_server_template = $wordpress::params::web_server_template,
+  $web_virtualhost     = $wordpress::params::web_virtualhost,
+  $db_type             = $wordpress::params::db_type,
+  $db_name             = $wordpress::params::db_name,
+  $db_host             = $wordpress::params::db_host,
+  $db_user             = $wordpress::params::db_user,
+  $db_password         = $wordpress::params::db_password,
+  $my_class            = $wordpress::params::my_class,
+  $source              = $wordpress::params::source,
+  $source_dir          = $wordpress::params::source_dir,
+  $source_dir_purge    = $wordpress::params::source_dir_purge,
+  $template            = $wordpress::params::template,
+  $options             = $wordpress::params::options,
+  $absent              = $wordpress::params::absent,
+  $monitor             = $wordpress::params::monitor,
+  $monitor_tool        = $wordpress::params::monitor_tool,
+  $monitor_target      = $wordpress::params::monitor_target,
+  $puppi               = $wordpress::params::puppi,
+  $puppi_helper        = $wordpress::params::puppi_helper,
+  $firewall            = $wordpress::params::firewall,
+  $firewall_tool       = $wordpress::params::firewall_tool,
+  $firewall_src        = $wordpress::params::firewall_src, 
+  $firewall_dst        = $wordpress::params::firewall_dst,
+  $debug               = $wordpress::params::debug,
+  $audit_only          = $wordpress::params::audit_only,
   $package             = $wordpress::params::package,   
   $config_dir          = $wordpress::params::config_dir,
   $config_file         = $wordpress::params::config_file,
@@ -150,7 +229,7 @@ class wordpress (
   $log_file            = $wordpress::params::log_file 
   ) inherits wordpress::params {
 
-  validate_bool($source_dir_purge, $absent , $monitor , $puppi , $debug)
+##  validate_bool($source_dir_purge , $absent , $monitor , $firewall , $debug, $audit_only)
 
   # Calculations of some variables used in the module
   $manage_package = $wordpress::absent ? {
@@ -171,6 +250,15 @@ class wordpress (
     }
   }
 
+  $manage_audit = $wordpress::audit_only ? {
+    true  => "all",
+    false => undef,
+  }
+
+  $manage_file_replace = $wordpress::audit_only ? {
+    true  => false,
+    false => true,
+  }
 
   # Installation is managed in dedicated class
   require wordpress::install
@@ -190,6 +278,8 @@ class wordpress (
       ''      => undef,
       default => template("$template"),
     },
+    replace => "${wordpress::manage_file_replace}",
+    audit   => $wordpress::manage_audit,
   }
 
   # Whole wordpress configuration directory can be recursively overriden
@@ -201,6 +291,8 @@ class wordpress (
       source  => $source_dir,
       recurse => true,
       purge   => $source_dir_purge,
+      replace => "${wordpress::manage_file_replace}",
+      audit   => $wordpress::manage_audit,
     }
   }
 
@@ -209,9 +301,25 @@ class wordpress (
     include $wordpress::my_class
   } 
 
+  # Include web server configuration if requested
+  if $wordpress::web_server {
+    stdlib42::webconfig { "$wordpress::web_virtualhost":
+      webserver          => $wordpress::web_server,
+      webserver_template => $wordpress::web_server_template,
+      virtualhost        => $wordpress::web_virtualhost,
+      document_root      => $wordpress::destination_dir,
+      access_log         => "$wordpress::log_dir/$wordpress::web_virtualhost-access.log",
+      error_log          => "$wordpress::log_dir/$wordpress::web_virtualhost-error.log",
+    }
+  } 
+
+  # Include database configuration, if db_type set 
+  if $wordpress::db_type == "mysql" {
+    include wordpress::mysql
+  } 
 
   # Provide puppi data, if enabled ( puppi => true )
-  if $wordpress::puppi == true { 
+  if any2bool($wordpress::puppi) == true { 
     $puppivars=get_class_args()
     file { "puppi_wordpress":
       path    => "${settings::vardir}/puppi/wordpress",
@@ -231,7 +339,7 @@ class wordpress (
       url     => "${wordpress::url_check}",
       pattern => "${wordpress::url_pattern}",
       port    => "${wordpress::port}",
-      target  => "${fqdn}",
+      target  => "${wordpress::params::monitor_target}",
       tool    => "${wordpress::monitor_tool}",
       enable  => $wordpress::manage_monitor,
     }
--- manifests/params.pp
+++ manifests/params.pp
@@ -14,36 +14,109 @@
 class wordpress::params {
 
   # Default installation type depends on OS package availability
-  $install = "package"
+  $install = $::wordpress_install ? {
+    ''      => "package",          # Default value
+    default => $::wordpress_install,
+  }
 
   # Install source from the upstream provider is updated to module's last update time
   # You may need to change this: use the "install_source" parameter of the wordpress class
-  $install_source = "http://wordpress.org/latest.zip"
+  $install_source = $::wordpress_install_source ? {
+    ''      => "http://wordpress.org/latest.zip",    # Default value
+    default => $::wordpress_install_source,
+  }
  
-  $install_destination = $operatingsystem ? {
-    /(?i:Debian|Ubuntu|Mint)/ => "/var/www",
-    /(?i:Suse|OpenSuse)/      => "/srv/www",
-    default                   => "/var/www/html",
+  $install_destination = $::wordpress_install_destination ? {
+    ''      => $operatingsystem ? {
+               /(?i:Debian|Ubuntu|Mint)/ => "/var/www",
+               /(?i:Suse|OpenSuse)/      => "/srv/www",
+               default                   => "/var/www/html",
+               }, # Default value calculated according to $operatingsystem 
+    default => $::wordpress_install_destination,
   }
 
-  $install_precommand  = ""
+  $install_precommand = $::wordpress_install_precommand ? {
+    ''      => "",                   # Default value
+    default => $::wordpress_install_precommand,
+  } 
 
-  $install_postcommand = ""
+  $install_postcommand = $::wordpress_install_postcommand ? {
+    ''      => "",                   # Default value
+    default => $::wordpress_install_postcommand,
+  }
 
-  $url_check           = ""
+  $url_check = $::wordpress_url_check ? {
+    ''      => "",                   # Default value
+    default => $::wordpress_url_check,
+  }
 
-  $url_pattern         = "OK"
+  $url_pattern = $::wordpress_url_pattern ? {
+    ''      => "OK",                 # Default value
+    default => $::wordpress_url_pattern,
+  } 
+
+  $web_server = $::wordpress_web_server ? {
+    ''      => $::web_server ? {
+      ""      => "",                 # Default value
+      default => $::web_server,
+    },
+    default => $::wordpress_web_server,
+  } 
+
+  $web_server_template = $::wordpress_web_server_template ? {
+    ''      => "",                   # Default value
+    default => $::wordpress_web_server_template,
+  }
+
+  $web_virtualhost = $::wordpress_web_virtualhost ? {
+    ''      => "wordpress.$domain",  # Default value
+    default => $::wordpress_web_virtualhost,
+  }
+
+  $db_type = $::wordpress_db_type ? {
+    ''      => "mysql",              # Default value
+    default => $::wordpress_db_type,
+  }
+
+  $db_host = $::wordpress_db_host ? {
+    ''      => "localhost",          # Default value
+    default => $::wordpress_db_host,
+  }
+
+  $db_name = $::wordpress_db_name ? {
+    ''      => "wordpress",          # Default value
+    default => $::wordpress_db_name,
+  }
+
+  $db_user = $::wordpress_db_user ? {
+    ''      => "wordpress",          # Default value
+    default => $::wordpress_db_user,
+  }
+
+  # We precalculate a random password
+  $wordpress_random_password = fqdn_rand(100000000000) 
+
+  $db_password = $::wordpress_db_password ? {
+    ''      => $wordpress_random_password,  # Default value
+    default => $::wordpress_db_password,
+  }
 
   $package = $operatingsystem ? {
     default => "wordpress",
   }
 
-  $config_dir = $operatingsystem ? {
-    default => "/etc/wordpress",
+  $config_dir = $wordpress::install ? {
+    package => $operatingsystem ? {
+      default => "/etc/wordpress",
+    },
+    default => "$install_destination",
   }
 
-  $config_file = $operatingsystem ? {
-    default => "/etc/wordpress/wp-config.php",
+  $config_file = $wordpress::install ? {
+    package => $operatingsystem ? {
+      default => "/etc/wordpress/wp-config.php",
+    },
+    default => "$install_destination/wp-config.php", 
   } 
 
   $config_file_mode = $operatingsystem ? { 
@@ -58,16 +131,119 @@ class wordpress::params {
     default => "root",
   }
 
-  $data_dir = $operatingsystem ? {
-    default => "/usr/share/wordpress",
+  $data_dir = $wordpress::install ? {
+    package => $operatingsystem ? {
+      default => "/usr/share/wordpress",
+    },
+    default => "$install_destination",
   }
 
   $log_dir = $operatingsystem ? {
-    default => "/var/log",
+    default => "",
   }
 
   $log_file = $operatingsystem ? {
-    default => "/var/log/wordpress.log",
+    default => "",
+  }
+
+
+  ### General variables that affect module's behaviour
+  # They can be set at top scope level or in a ENC
+
+  $my_class = $::wordpress_my_class ? {
+    ''      => "",                      # Default value
+    default => $::wordpress_my_class,
+  }
+
+  $source = $::wordpress_source ? {
+    ''      => "",                      # Default value
+    default => $::wordpress_source,
+  }
+
+  $source_dir = $::wordpress_source_dir ? {
+    ''      => "",                      # Default value
+    default => $::wordpress_source_dir,
+  }
+
+  $source_dir_purge = $::wordpress_source_dir_purge ? {
+    ''      => false,                   # Default value
+    default => $::wordpress_source_dir_purge,
+  }
+
+  $template = $::wordpress_template ? {
+    ''      => "wordpress/wp-config.php", # Default value
+    default => $::wordpress_template,
+  }
+
+  $options = $::wordpress_options ? {
+    ''      => "",                      # Default value
+    default => $::wordpress_options,
+  }
+
+  $absent = $::wordpress_absent ? {
+    ''      => false,                   # Default value
+    default => $::wordpress_absent,
+  }
+
+
+
+  ### General module variables that can have a site or per module default
+  # They can be set at top scope level or in a ENC
+
+  $monitor = $::wordpress_monitor ? {
+    ''      => $::monitor ? {
+      ''      => false,                # Default value
+      default => $::monitor,
+    },
+    default => $::wordpress_monitor,
+  }
+
+  $monitor_tool = $::wordpress_monitor_tool ? {
+    ''      => $::monitor_tool ? {
+      ''      => "",                   # Default value
+      default => $::monitor_tool,
+    },
+    default => $::wordpress_monitor_tool,
+  }
+
+  $monitor_target = $::wordpress_monitor_target ? {
+    ''      => $::monitor_target ? {
+      ''      => "$ipaddress",         # Default value
+      default => $::monitor_target,
+    },
+    default => $::wordpress_monitor_target,
+  }
+
+  $puppi = $::wordpress_puppi ? {
+    ''      => $::puppi ? {
+      ''      => false,                # Default value
+      default => $::puppi,
+    },
+    default => $::wordpress_puppi,
+  }  
+
+  $puppi_helper = $::wordpress_puppi_helper ? {
+    ''      => $::puppi_helper ? {
+      ''      => "standard",           # Default value
+      default => $::puppi_helper,
+    },
+    default => $::wordpress_puppi_helper,
+  }
+
+  $debug = $::wordpress_debug ? {
+    ''      => $::debug ? {
+      ''      => false,                # Default value
+      default => $::debug,
+    },
+    default => $::wordpress_debug,
+  }
+
+  $audit_only = $::wordpress_audit_only ? {
+    ''      => $::audit_only ? {
+      ''      => false,                # Default value
+      default => $::audit_only,
+    },
+    default => $::wordpress_audit_only,
   }
 
 }
