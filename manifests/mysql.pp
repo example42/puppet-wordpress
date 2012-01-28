e41e8b14f0f3cb1ef3090bc3743ef1a266e9fc33
Alessandro Franceschi <al@lab42.it>
2012-01-28 10:45:35 +0100

    Lints

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -2,30 +2,32 @@
 #
 # This class configures mysql for wordpress installation
 #
-# == Usage 
-# 
-# This class is not intended to be used directly. It's automatically included by wordpress
+# == Usage
+#
+# This class is not intended to be used directly.
+# It's automatically included by wordpress
 #
 class wordpress::mysql inherits wordpress {
 
   case $wordpress::params::db_host {
-    "localhost","127.0.0.1": {
-      mysql::grant { "wordpress_server_grants_${fqdn}":
-        mysql_db         => "${wordpress::params::db_name}",
-        mysql_user       => "${wordpress::params::db_user}",
-        mysql_password   => "${wordpress::params::db_password}",
-        mysql_privileges => "ALL",
-        mysql_host       => "${wordpress::params::db_host}",
+    'localhost','127.0.0.1': {
+      mysql::grant { "wordpress_server_grants_${::fqdn}":
+        mysql_db         => $wordpress::params::db_name,
+        mysql_user       => $wordpress::params::db_user,
+        mysql_password   => $wordpress::params::db_password,
+        mysql_privileges => 'ALL',
+        mysql_host       => $wordpress::params::db_host,
       }
     }
     default: {
-      # Automanagement of Mysql grants on external servers requires StoredConfigs.
-      @@mysql::grant { "wordpress_server_grants_${fqdn}":
-        mysql_db         => "${wordpress::params::db_name}",
-        mysql_user       => "${wordpress::params::db_user}",
-        mysql_password   => "${wordpress::params::db_password}",
-        mysql_privileges => "ALL",
-        mysql_host       => "${fqdn}",
+      # Automanagement of Mysql grants on external servers
+      # requires StoredConfigs.
+      @@mysql::grant { "wordpress_server_grants_${::fqdn}":
+        mysql_db         => $wordpress::params::db_name,
+        mysql_user       => $wordpress::params::db_user,
+        mysql_password   => $wordpress::params::db_password,
+        mysql_privileges => 'ALL',
+        mysql_host       => $::fqdn,
         tag              => "mysql_grants_${wordpress::params::db_host}",
       }
     }
